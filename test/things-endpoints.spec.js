7a723c562a3f2d0637db8f752198467cb54d9699
unknown <sallyannegaller@gmail.com>
2020-12-24 14:07:06 -0800

    Added secure login and tests

--- test/things-endpoints.spec.js
+++ test/things-endpoints.spec.js
@@ -122,6 +122,7 @@ describe("Things Endpoints", function () {
   describe(`GET /api/things/:thing_id/reviews`, () => {
     context(`Given no things`, () => {
       beforeEach(() => helpers.seedUsers(db, testUsers));
+
       it(`responds with 404`, () => {
         const thingId = 123456;
         return supertest(app)
